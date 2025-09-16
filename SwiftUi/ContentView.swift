d8ae3e049b0ba2f72b2fd9d0e48a09735abb1382
Mallikharjuna-001 <arjun2281993@gmail.com>
2025-09-16 11:14:24 +0530

    Initial Commit

---
+++ SwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUi
+//
+//  Created by Mallikharjuna on 16/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUi/SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiApp.swift
+//  SwiftUi
+//
+//  Created by Mallikharjuna on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
